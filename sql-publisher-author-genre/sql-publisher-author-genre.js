722057cf210fa0cd7bed0a721c258c8732d05130
maramalquraan <quraanmaram@gmail.com>
2017-09-23 09:30:40 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -9,11 +9,23 @@ var Schemas = {
   OPTION_6: 'option 6'
 };
 
-var sqlPublisherAuthorSchema = 
+var sqlPublisherAuthorSchema = Schemas.OPTION_6;
 // TODO: Write a comment here explaining your choice
+//the Option 6 because we have a many to many relationship
+// the publisher can has many authors and also the author may has many publishers
+// so we have a foreign key in both of them.
+// foreign keys are: publisher ID , Author Id
 
-var sqlPublisherGenreSchema = 
-// TODO: Write a comment here explaining your choice
 
-var sqlAuthorGenreSchema = 
+var sqlPublisherGenreSchema =Schemas.OPTION_4; 
 // TODO: Write a comment here explaining your choice
+//the Option 4 because the relatioship between them it's 1 to many
+// so the Publisher has a foreign key which is the gener Id 
+// 1 publisher can has many geners. 
+
+var sqlAuthorGenreSchema = Schemas.OPTION_6;
+// TODO: Write a comment here explaining your choice
+//the Option 6 because we have a many to many relationship
+// the author can has many genres and also the genre may has many authors
+// so we have a foreign key in both of them.
+//  foreign keys are: genre ID , Author Id.
